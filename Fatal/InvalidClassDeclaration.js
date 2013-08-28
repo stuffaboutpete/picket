e7ab3df8e96f6c812e19a79b95ee005baa7d55dc
Pete Smith <pete@subrota.com>
2013-08-28 15:06:03 +0100

    Updates format of fatals to match master

--- Fatal/InvalidClassDeclaration.js
+++ Fatal/InvalidClassDeclaration.js
@@ -1,5 +1,10 @@
 ;(function(undefined){
 	
-	InvalidClassDeclarationFatal = function(){};
+	InvalidClassDeclarationFatal = function(message){
+		this.message = message;
+		this.stack = Error().stack;
+	};
+	InvalidClassDeclarationFatal.prototype = Object.create(Error.prototype);
+	InvalidClassDeclarationFatal.prototype.name = 'Fatal: InvalidClassDeclaration';
 	
 })();
