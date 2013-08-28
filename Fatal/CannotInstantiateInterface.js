2e681572aa26076b26fd816cfca56b633ab6be3a
Pete Smith <pete@subrota.com>
2013-08-28 14:53:33 +0100

    Reformats fatal objects
    Fatals now show up in the console as errors of type Fatal with full
    stack trace and well formatted messages.

--- Fatal/CannotInstantiateInterface.js
+++ Fatal/CannotInstantiateInterface.js
@@ -1,5 +1,10 @@
 ;(function(undefined){
 	
-	CannotInstantiateInterfaceFatal = function(){};
+	CannotInstantiateInterfaceFatal = function(message){
+		this.message = message;
+		this.stack = Error().stack;
+	};
+	CannotInstantiateInterfaceFatal.prototype = Object.create(Error.prototype);
+	CannotInstantiateInterfaceFatal.prototype.name = 'Fatal: CannotInstantiateInterface';
 	
 })();
--- Fatal/UnknownProperty.js
+++ Fatal/UnknownProperty.js
@@ -1,5 +1,10 @@
 ;(function(undefined){
 	
-	UnknownPropertyFatal = function(){};
+	UnknownPropertyFatal = function(message){
+		this.message = message;
+		this.stack = Error().stack;
+	};
+	UnknownPropertyFatal.prototype = Object.create(Error.prototype);
+	UnknownPropertyFatal.prototype.name = 'Fatal: UnknownProperty';
 	
 })();
